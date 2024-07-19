a30dacdc8b5bcbebc01304d55e744fbfa48a3466
ucsaeki <ddt.ys.saeki@gmail.com>
2024-07-19 20:27:48 +0900

    shuusei

--- src/Work7/FantanHand.java
+++ src/Work7/FantanHand.java
@@ -7,31 +7,14 @@ package Work7;
  * 作成日 2024/07/02
  */
 public class FantanHand extends Hand {
-
-	public FantanHand() {
-		
-	}
-	
-	public void addCard() {
-		
-		
-	}
-	
-	public Card pickCard() {
-		
-		
-	}
-	
-	public void shuffleCard() {
-		
-		
-	}
-	
-	public int getNumberOfCards() {
-		
-		
-	}
-	
+	/* 
+	 * 関数名 lookCard
+	 * 概要 カードを見る
+	 * 引数 なし
+	 * 返り値 なし
+	 * 作成者 Y.Saeki
+	 * 作成日 2024/07/02
+	 */
 	public void lookCard() {
 		
 		
